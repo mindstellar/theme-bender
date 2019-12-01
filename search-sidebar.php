7a0218da7ad31fb2f8dd78f5e64b1bd1e47da59b
Navjot Tomer <navjottomer@gmail.com>
2019-12-01 22:12:48 +0530

    Fix

--- search-sidebar.php
+++ search-sidebar.php
@@ -19,13 +19,10 @@
      * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
      */
 
-    $category = __get('category');
-    if(!isset($category['pk_i_id']) ) {
-        if(!is_array($category)){
-            $category = [];
-        }
-        $category['pk_i_id'] = null;
-    }
+$category = (array)__get('category');
+if (!isset($category['pk_i_id'])) {
+    $category['pk_i_id'] = null;
+}
 ?>
 <div id="sidebar">
 <?php osc_alert_form(); ?>
